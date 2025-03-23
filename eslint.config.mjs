1ae9587167a1bddcbf4ae794ada02c725acaf239
Niroasd <123634396+Niroasd@users.noreply.github.com>
2025-03-23 23:32:56 +0200

    add jest globals to linter cfg

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,15 @@ import pluginReact from "eslint-plugin-react";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,jsx}"] },
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
+  { 
+    files: ["**/*.{js,mjs,cjs,jsx}"], 
+    languageOptions: { 
+      globals: {
+        ...globals.browser,
+        ...globals.jest
+      } 
+    } 
+  },
   { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
   pluginReact.configs.flat.recommended,
   {
